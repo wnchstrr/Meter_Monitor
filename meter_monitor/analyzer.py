74da65e46b6fdf844778ef5f81b3ef3b76de1152
wnchstrr <haligali1592@gmail.com>
2026-05-10 12:10:36 +0500

    feat: add Analyzer

---
+++ meter_monitor/analyzer.py
@@ -0,0 +1,17 @@
+from models import Reading
+
+
+class Analyzer:
+
+    def calculate_consumption(self, readings: list[Reading]) -> list[dict]:
+        result = []
+        for i in range(1, len(readings)):
+            current = readings[i]
+            previous = readings[i -1]
+            consumption = current.value - previous.value
+            result.append({
+                "date": current.date,
+                "meter_type": current.meter_type,
+                "consumption": consumption, 
+            })
+        return result 
