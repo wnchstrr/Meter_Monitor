a7b278056a7a0dbd1857fdebe307d0e0e3a21594
wnchstrr <haligali1592@gmail.com>
2026-05-16 20:59:24 +0500

    feat: add ui functions

---
+++ meter_monitor/ui.py
@@ -0,0 +1,20 @@
+def show_menu():
+    """Показывает меню"""
+    print("0. Выход")
+    print("1. Добавить показания")
+    print("2. Удалить показания")
+    print("3. Показать все показания")
+    print("4. Фильтр по периоду")
+    print("5. Показать график")
+
+def get_choice():
+    """Принимает выбор пользователя"""
+    return input("Выберите пункт из меню: ")
+
+def show_readings(readings: list[Reading]) -> None:
+    if not readings:
+        print("Показаний нет.")
+        return
+    for index, reading in enumerate(readings, start=1):
+        print(f"{index}. [{reading.meter_type}, {reading.value}, {reading.date}]")
+
